4184962493b2410f30a044fcbd3061c5bd8fff92
Juan manuel Pedro Villalba <xomie_01@hotmail.com>
2018-02-22 13:15:59 -0300

    Update status.sh

--- status.sh
+++ status.sh
@@ -1,3 +1,11 @@
+#! /bin/bash
+# Author: Villalba Juan Manuel Pedro
+# Version: 0.0.0.1
+# Estado: Alpha
+# Destino: resolver problemas or solved problem's APT-GET
+# Date: 05:41pm 14-09-2017
+# Advertencia: Usar con cautela y bajo propia responsabilidad
+# Status alpha development, to issues fail's to system mysql.
  service mysql status verbose > status.log &&\
  cat status.log | prisma.py
  date | prisma.py
